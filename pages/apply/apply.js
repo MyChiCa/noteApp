bfe3147394377cf32ba40e6c9c20473c51932685
zhushunsheng <503305196@qq.com>
2017-11-24 17:53:06 +0800

    申请页面表单

--- pages/apply/apply.js
+++ pages/apply/apply.js
@@ -3,6 +3,7 @@ const date = new Date();
 const start = util.formatDate(date);
 Page({
   data: {
+    teamId:'',//团队Id
     name:'', //真是姓名
     position:'', //职位
     phone:'', //手机号
@@ -13,7 +14,13 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    if (options.teamId){
+        this.setData({
+          teamId: options.teamId
+        });
+    }else{
+      //获取不到teamId 可以跳转回首页
+    }
   },
 
   /**
@@ -61,5 +68,23 @@ Page({
     this.setData({
       date: e.detail.value
     })
+  },
+  formSubmit(e){
+    var apply = e.detail.value;
+     //验证表单
+    if (apply.name == "" || apply.position == ""){
+      wx.showToast({
+        title: '请完善表单'
+      })
+    } else if (apply.phone.length != 11){
+      wx.showToast({
+        title: '11位手机'
+      })
+    }else{
+        //表单验证通过
+        apply.teamId = this.data.teamId;
+        apply.userId = "11111";//用户id
+        console.log(apply);
+    }
   }
 })
